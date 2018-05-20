0fcb23a0a667c97a3efbf5ff518fdacee785e0af
mtrelinski <mtrelinski@interia.pl>
2018-05-20 14:06:49 +0200

    First commit :)

---
+++ Szop_v1.0/src/com/szop/controller/IndexController.java
@@ -0,0 +1,26 @@
+package com.szop.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class IndexController {
+
+	
+	@RequestMapping("/index")
+	public ModelAndView indexAction() {
+		
+		ModelAndView mav = new ModelAndView("index");
+		
+		System.out.println("--------------------------------> test test");
+		
+		String message = "Dupa index sklepu";
+		
+		mav.addObject("message", message);
+		
+		return mav;
+	}
+	
+	
+}
